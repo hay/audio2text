9fe2093f551bc51e4d58d2a4f1e54067ce60a196
Hay Kranen <hay@haykranen.nl>
2023-03-16 20:37:18 +0100

    This seems to work

--- audio2text.py
+++ audio2text.py
@@ -1,17 +1,57 @@
+from pathlib import Path
+import ffmpeg
+from shortuuid import uuid
 import subprocess
 
 class AudioToText:
-    def __init__(self, model_path,
+    def __init__(self,
+        model_path,
         language = "en",
         output_type = "vtt",
-        processors = 1
+        processors = 1,
+        verbose = False,
+        diarize = False
     ):
         self.model_path = model_path
         self.language = language
         self.output_type = output_type
         self.processors = processors
+        self.verbose = verbose
+        self.diarize = diarize
+        self._log(f"Initialized AudioToText")
+
+    def _log(self, msg):
+        if self.verbose:
+            print(msg)
+
+    def convert(self, in_path):
+        tmp_file = Path("tmp") / f"{uuid()}.wav"
+
+        self._log(f"Converting {in_path} to {tmp_file}")
+
+        try :
+            (
+                ffmpeg.input(in_path)
+                .output(
+                    str(tmp_file.resolve()),
+                    acodec = "pcm_s16le",
+                    ac = 2,
+                    ar = 16000
+                ).run(quiet = True)
+            )
+        except ffmpeg.Error as e:
+            raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}")
+
+        return tmp_file
 
     def transcribe(self, in_path, out_path):
+        self._log(f"Transcribing {in_path} as {out_path}")
+        tmp_file = self.convert(in_path)
+        self.transcribe_processed_wav(tmp_file, out_path)
+        self._log(f"Removing tmp file {tmp_file}")
+        tmp_file.unlink()
+
+    def transcribe_processed_wav(self, in_path, out_path):
         cmd = [
             "./whispercpp",
             "-m", str(self.model_path.resolve()),
@@ -22,6 +62,9 @@ class AudioToText:
             "-p", str(self.processors)
         ]
 
+        if self.diarize:
+            cmd.append("--diarize 1")
+
         command = " ".join(cmd)
-        print(command)
+        self._log(f"Executing whisper command '{command}'")
         subprocess.check_call(command, shell = True)
--- cli.py
+++ cli.py
@@ -4,26 +4,37 @@ from pathlib import Path
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file", help = "File to parse")
-parser.add_argument('-v', '--verbose', action="store_true")
+parser.add_argument("-di", "--diarize",
+    help = "Diarize audio (only works for natural stereo audio)",
+    action = "store_true"
+)
+parser.add_argument("-i", "--input", help = "File to parse")
+parser.add_argument("-l", "--language")
+parser.add_argument("-m", "--model-path",
+    help = "Path to model",
+    default = Path("models") / "ggml-large.bin"
+)
 parser.add_argument("-o", "--output")
 parser.add_argument("-of", "--output-format",
     choices = ["txt", "vtt", "srt", "csv", "words"],
     default = "srt"
 )
+parser.add_argument('-v', '--verbose', action="store_true")
 
 args = parser.parse_args()
 
-if not args.file or not args.output:
+if not args.input or not args.output:
     parser.print_help()
 else:
     a2t = AudioToText(
-        model_path = Path("models") / "ggml-large.bin",
-        language = "nl",
-        output_type = args.output_format
+        diarize = args.diarize,
+        model_path = args.model_path,
+        language = args.language,
+        output_type = args.output_format,
+        verbose = args.verbose
     )
 
-    in_path = Path(args.file)
+    in_path = Path(args.input)
     out_path = Path(args.output)
 
     a2t.transcribe(in_path, out_path)
