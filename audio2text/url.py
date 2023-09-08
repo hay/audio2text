03ab61a2765886754182d6bc0c9a73c8f9a4417e
Sahra <60785629+sahramohamed@users.noreply.github.com>
2023-09-08 13:24:37 +0200

    Update url.py
    Adding a timeout for URL retrieval

--- audio2text/url.py
+++ audio2text/url.py
@@ -1,6 +1,7 @@
 from urllib.request import urlretrieve
 from .file import get_tmp_file_path
 import logging
+import requests
 
 logger = logging.getLogger(__name__)
 
@@ -8,6 +9,6 @@ def download_tmp_file(url):
     path = get_tmp_file_path(suffix = ".mp3")
 
     logger.debug(f"Downloading <{url}> to {path}")
-
+    requests.get(url, timeout=60)
     urlretrieve(url, path)
     return path
