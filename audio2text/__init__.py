858e52aba726a793b639743622fa8e5f5259d389
Sahra <60785629+sahramohamed@users.noreply.github.com>
2023-09-08 13:19:47 +0200

    Update __init__.py
    Changing the location of the temporarily downloaded mp3 files to the local scratch memory of Snellius.

--- audio2text/__init__.py
+++ audio2text/__init__.py
@@ -1,7 +1,9 @@
+import os
+
 DEFAULT_LANG = "auto" # Auto detect
 DEFAULT_OUTPUT_TYPE = "srt"
 DEFAULT_PROCESSOR_COUNT = 1
 DEFAULT_SRTPARSER_OUTPUT = "csv"
 SRTPARSER_OUTPUT_FORMATS = ["csv", "json", "txt"]
-TMP_FILE_FOLDER = "tmp"
+TMP_FILE_FOLDER = os.environ['TMPDIR']
 WHISPER_OUTPUT_FORMATS = ["txt", "vtt", "srt", "words", "csv"]
